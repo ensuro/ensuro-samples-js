d19261f7494188046ca89452a0b0a8a61a650c8e
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2021-12-28 19:33:16 -0300

    Use yargs and keep json files outside of ensuro.js

--- ensuro.js
+++ ensuro.js
@@ -1,7 +1,6 @@
 const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
 const { ethers } = require('ethers');
 
-const helper = require('./helper.js');
 const _BN = ethers.BigNumber.from;
 const WAD = _BN(1e10).mul(_BN(1e8));  // 1e10*1e8=1e18
 const RAY = WAD.mul(_BN(1e9));  // 1e18*1e9=1e27
@@ -12,42 +11,67 @@ const NEW_POLICY_EVENT = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(
     "NewPolicy(address,(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,address,uint40,uint40))"
 ));
 
-function _W(value){
-    if (ethers.BigNumber.isBigNumber(value)){
-        return value;
-    }
-    if (!Number.isInteger(value))
-        return _BN(Math.round(value * 1e6));
-    return _BN(value).mul(_BN(1e6));
-}
-    
-function _R(value){
-    if (ethers.BigNumber.isBigNumber(value)){
-        return value;
-    }
-    if (!Number.isInteger(value))
-        return _BN(value * 1e9).mul(WAD);
-    return _BN(value).mul(RAY);
+function amountDecimals() {
+  // Decimals must be at least 6
+  let decimals = Number.parseInt(process.env.AMOUNT_DECIMALS || "6");
+  console.assert(decimals >= 6);
+  return decimals;
 }
 
-async function newPolicy(data, customer, rm){
-    const tx = await rm.newPolicy(_W(data.payout), _W(data.premium), _R(data.lossProb), data.expiration, customer, 
-        {gasLimit: 999999} // This is to force sending transactions that will fail (to see the error in the 
-                            // transaction - remove in production
-    );
-    console.log(`Transaction created: ${tx.hash}`);
+/**
+ * Function used to convert amounts - Takes int or float and returns a BigNumber with fixed decimals
+ *
+ * @param {value} Value to convert - If already BigNumber doesn't do anything
+ */
+function _A(value) {
+  if (ethers.BigNumber.isBigNumber(value)){
+      return value;
+  }
+  if (typeof value === 'string' || value instanceof String) {
+    return _BN(value).mul(_BN(Math.pow(10, amountDecimals())));
+  } else {
+    return _BN(Math.round(value * 1e6)).mul(_BN(Math.pow(10, amountDecimals() - 6)));
+  }
+}
 
-    const receipt = await tx.wait();
+
+/**
+ * Function used to convert to RAY (used for ratios, percentages, etc.)
+ *
+ * @param {value} Value to convert - If already BigNumber doesn't do anything
+ */
+function _R(value) {
+  if (ethers.BigNumber.isBigNumber(value)){
+    return value;
+  }
+  if (!Number.isInteger(value))
+    return _BN(value * 1e9).mul(WAD);
+  return _BN(value).mul(RAY);
+}
+
+async function newPolicy(data, customer, rm) {
+  let expiration = data.expiration;
+  if (expiration < 1600000000) {
+    // it's a relative expiration timestamp;
+    expiration = Math.floor(Date.now() / 1000) + expiration;
+  }
+  const tx = await rm.newPolicy(_A(data.payout), _A(data.premium), _R(data.lossProb), expiration, customer,
+    {gasLimit: 999999} // This is to force sending transactions that will fail (to see the error in the
+                        // transaction - remove in production
+  );
+  console.debug(`Transaction created: ${tx.hash}`);
+  return tx;
+}
+
+function decodeNewPolicyReceipt(receipt) {
     const events = receipt.logs.filter(e => e.topics[0] === NEW_POLICY_EVENT);
 
-    policy = helper.parsePolicyData(events[0].data);
-    helper.writePolicyData(policy);
-    return events[0].data;
+    return parsePolicyData(events[0].data);
 }
 
 async function resolve(policyData, customerWon, rm){
-    const tx = await rm.resolvePolicyFullPayout(policyData, customerWon, 
-        {gasLimit: 999999} // This is to force sending transactions that will fail (to see the error in the 
+    const tx = await rm.resolvePolicyFullPayout(policyData, customerWon,
+        {gasLimit: 999999} // This is to force sending transactions that will fail (to see the error in the
                             // transaction - remove in production
     );
     console.log(`Transaction created: ${tx.hash}`);
@@ -61,6 +85,65 @@ function getDefenderRelaySigner(API_KEY, API_SECRET) {
     return signer;
 }
 
+function parsePolicyData(hexPolicyData){
 
-module.exports = {newPolicy, resolve, getDefenderRelaySigner, _W, _R };
+    let policyData = {}
+    let data = []
+    let id = hexPolicyData.substring(0,66);
+    const policyId = parseInt(id, 16);
+    data.push(policyId);
+    policyData.id = policyId;
+
+    let payout = '0x' + hexPolicyData.substring(66,130);
+    data.push(payout);
+    policyData.payout = payout;
+
+    let premium = '0x' + hexPolicyData.substring(130,194);
+    data.push(premium);
+    policyData.premium = premium;
+
+    let scr = '0x' + hexPolicyData.substring(194,258);
+    data.push(scr);
+    policyData.scr = scr;
+
+    let lossProb = '0x' + hexPolicyData.substring(258,322);
+    data.push(lossProb);
+    policyData.lossProb = lossProb;
+
+    let purePremium = '0x' + hexPolicyData.substring(322,386);
+    data.push(purePremium);
+    policyData.purePremium = purePremium;
+
+    let premiumForEnsuro = '0x' + hexPolicyData.substring(386,450);
+    data.push(premiumForEnsuro);
+    policyData.premiumForEnsuro = premiumForEnsuro;
+
+    let premiumForRm = '0x' + hexPolicyData.substring(450,514);
+    data.push(premiumForRm);
+    policyData.premiumForRm = premiumForRm;
+
+    let premiumForLps = '0x' + hexPolicyData.substring(514,578);
+    data.push(premiumForLps);
+    policyData.premiumForLps = premiumForLps;
+
+    let address = '0x' + hexPolicyData.substring(602,642);
+    data.push(address);
+    policyData.customer = address;
+
+    let start = '0x' + hexPolicyData.substring(696,706);
+    data.push(start);
+    policyData.start = start;
+
+    let expiration = '0x' + hexPolicyData.substring(760,770);
+    data.push(expiration);
+    policyData.expiration = expiration;
+
+    policyData.data = data;
+    return policyData;
+}
+
+module.exports = {
+    newPolicy, resolve, getDefenderRelaySigner, _A, _R, decodeNewPolicyReceipt, parsePolicyData,
+    PRICER_ROLE, RESOLVER_ROLE, NEW_POLICY_EVENT
+};
 
--- index.js
+++ index.js
@@ -2,18 +2,56 @@
 
 const { ethers } = require('ethers');
 const { abi } = require("./abis/TrustfulRiskModule.json");
+const fs = require('fs');
 
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 const RM_ADDRESS = process.env.RM_ADDRESS;
 
 const yargs = require('yargs');
-const helper = require('./helper.js');
 const ensuro = require('./ensuro.js');
 const signer = ensuro.getDefenderRelaySigner(API_KEY, API_SECRET);
 
+async function newPolicyCommand(argv) {
+  const rm = new ethers.Contract(argv.rmAddress, abi, signer);
+  const policyData = JSON.parse(fs.readFileSync(argv.policyData));
+  const tx = await ensuro.newPolicy(policyData, argv.customer, rm);
+  // The transaction was sent to the blockchain, might take sometime to approve, to
+  // avoid sending duplicated policies, it's better to save the {tx.hash} and check asyncronously
+  // the result of the transaction
+  const receipt = await tx.wait();
+  const createPolicyData = ensuro.decodeNewPolicyReceipt(receipt);
+  console.log(`New Policy was created with id: ${createPolicyData.id}`);
+  const outputFile = argv.outputFile || `./PolicyData-${createPolicyData.id}.json`;
+  fs.writeFile(outputFile, JSON.stringify(createPolicyData, null, 4), (err) => {
+        if (err) {  console.error(err);  return; };
+  });
+}
 
-const rm = new ethers.Contract(RM_ADDRESS, abi, signer);
+yargs.scriptName("ensuro-cli")
+  .usage('$0 <cmd> [args]')
+  .command('new-policy <policyData> <customer> [--rm <rm-address>]', 'Create new policy', (yargs) => {
+    yargs.positional('policyData', {
+      type: 'string',
+      describe: 'Json file with the data of the policy to be created - See sample-policy.json'
+    });
+    yargs.positional('customer', {
+      type: 'string',
+      describe: 'Address that pays the premium and receives the payout'
+    });
+    yargs.option("rmAddress", {
+      describe: "Address of the RiskModule contract ",
+      default: RM_ADDRESS
+    });
+    yargs.option("outputFile", {
+      describe: "Output file where the policy data will be saved (json)",
+    });
+  }, newPolicyCommand)
+  .help()
+  .argv;
+
+/*
+
 (async ()=>{
 
     const args = yargs.argv;
@@ -35,4 +73,4 @@ const rm = new ethers.Contract(RM_ADDRESS, abi, signer);
       await ensuro.resolve(data.data, true, rm);
     }
 
-})();
+})();*/
